aab3e07980bafb5a7cd1cca3f6e4221ee71720ab
Heder M. David <116081626+hederdavid@users.noreply.github.com>
2023-08-18 13:24:51 -0300

    exercicios da documentação vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import makeMeRed from './components/makeMeRed.vue';
+import EventListener from './components/EventListener.vue'
+import FormBindings from './components/FormBindings.vue'
+import ConditionalRendering from './components/ConditionalRendering.vue'
+</script>
+
+<template>
+  <div>
+    <makeMeRed/>
+    <EventListener/>
+    <FormBindings/>
+    <ConditionalRendering/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FormBindings.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+
+</script>
+
+<template>
+  <input v-model="text" placeholder="Type here">
+  <p>{{ text }}</p>
+</template>
